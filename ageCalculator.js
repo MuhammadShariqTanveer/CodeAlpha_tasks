e0e7ec867f5d9be73ffa65ffe271918aa5217359
Muhammad Shariq Tanveer <136991351+MuhammadShariqTanveer@users.noreply.github.com>
2024-07-21 13:31:06 +0500

    Task

--- ageCalculator.js
+++ ageCalculator.js
@@ -1,5 +1,20 @@
+function validateDOB() {
+    const dob = document.getElementById('dob').value;
+    const button = document.getElementById('calculate-button');
+
+    if (dob) {
+        button.classList.remove('button-error');
+        button.classList.add('button-valid');
+    } else {
+        button.classList.remove('button-valid');
+        button.classList.add('button-error');
+    }
+}
+
+
 function calculateAge() {
     const dob = document.getElementById('dob').value;
+    const button = document.getElementById('button');
     if (dob) {
         const dobDate = new Date(dob);
         const today = new Date();
@@ -14,5 +29,8 @@ function calculateAge() {
         document.getElementById('result').textContent = `You are ${age} years old.`;
     } else {
         document.getElementById('result').textContent = 'Please enter your date of birth.';
+        document.getElementById('result').classList.add('error');
     }
+
+  
 }
